0e38f3b76113b599090872e4994ad0fea0dc1775
Takehiro Adachi <takehiro0740@gmail.com>
2013-02-18 19:06:17 +0900

    HEROKU_URL -> HEROKU_GIT_URL
    This change is necessary since when we're using hipchat adapter heroku
    free plan, we need HEROKU_URL=http://appname.heroku.com configured.
    (ref
    https://github.com/hipchat/hubot-hipchat#quickstart-hubot-for-hipchat-on
    -heroku)
    So we need to name the git url something other then HEROKU_URL.

--- git-across.sh
+++ git-across.sh
@@ -4,7 +4,7 @@ if [ "run" != "$1" ]; then
   exec ssh -i "$IDENTITY_FILE" -o "StrictHostKeyChecking no" "$@"
 fi
 
-echo "Deploying from $GITHUB_URL to $HEROKU_URL"
+echo "Deploying from $GITHUB_URL to $HEROKU_GIT_URL"
 
 self="$( cd "$( dirname "$0" )" && pwd )"/$0
 
@@ -19,7 +19,7 @@ echo "$GITHUB_PUBLIC_KEY" >"$IDENTITY_FILE.pub"
 
 git clone $GITHUB_URL $repo
 cd $repo
-git remote add heroku $HEROKU_URL
+git remote add heroku $HEROKU_GIT_URL
 git push heroku
 
 rm -f "$IDENTITY_FILE"
